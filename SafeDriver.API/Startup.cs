0ab590a3bfb0bd5b07875fe90af0e6fcbb9c5bbf
g-leme-c <gabriellcalsada@gmail.com>
2021-09-20 13:49:00 -0300

    Added CORS config.

--- SafeDriver.API/Startup.cs
+++ SafeDriver.API/Startup.cs
@@ -29,6 +29,10 @@ namespace SafeDriver.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(c => {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            });
+            
             ConfigureDbContext(services);
             ConfigureSwaggerService(services);
             ConfigureAutoMapping(services);
