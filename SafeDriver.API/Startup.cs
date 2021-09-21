757d42316ae65572498201aed192d4ef70710508
g-leme-c <gabriellcalsada@gmail.com>
2021-09-21 17:46:16 -0300

    Added useCors.

--- SafeDriver.API/Startup.cs
+++ SafeDriver.API/Startup.cs
@@ -42,6 +42,8 @@ namespace SafeDriver.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options.AllowAnyOrigin());
+
             app.UseSwagger();
             app.UseSwaggerUI(c => {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "SafeDriverAPI");
